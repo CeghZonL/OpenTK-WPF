5d3ce0ebf6c507be2751445369b72cf2f811d329
CeghZonL <jackacheng@outlook.com>
2019-09-21 10:31:19 +0800

    first commit

---
+++ OpenTK WPF/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+
+namespace OpenTK_WPF
+{
+    
+
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// MainWindow.xaml 的交互逻辑
+        /// </summary>
+
+        #region Field
+
+        private int _vertexBufferObject;
+        private int _vertexArrayObject;
+        private Shader _shader;
+
+
+        #endregion
+
+        #region Property
+
+        float[] vertices = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     -0.5f,  0.5f, 0.0f,
+     0.5f, 0.5f, 0.0f
+
+};
+        #endregion
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void GLControl_Paint(object sender, PaintEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            //
+            _shader.Use();
+
+            //
+            GL.BindVertexArray(_vertexArrayObject);
+
+            //
+            GL.DrawArrays(PrimitiveType.TriangleStrip, 0, 4);
+
+            //
+            _glControl.SwapBuffers();
+        }
+
+        private void GLControl_Load(object sender, EventArgs e)
+        {            
+            GL.ClearColor(Color4.Red);
+            //
+            _vertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            //
+            _shader = new Shader("Shaders/VertexShader.GLSL", "Shaders/FragmentShader.GLSL");
+
+            //
+            _shader.Use();
+
+            //
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+
+            //
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            //
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+        }
+
+        //
+        protected void OnUnload(EventArgs e)
+        {
+            //
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+
+            //
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(_vertexBufferObject);
+        }
+
+        private void _glControl_Resize(object sender, EventArgs e)
+        {
+            GL.Viewport(0, 0, _glControl.Width, _glControl.Height);
+        }
+    }
+}
---
+++ OpenTK WPF/Shader.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace OpenTK_WPF
+{
+    public class Shader
+    {
+        #region Field
+
+        readonly int Handle;
+        readonly int _vertexShader;
+        readonly int _fragmentShader;
+        private readonly Dictionary<string, int> _uniformLocations;
+        #endregion
+
+
+        //public Shader(string vertexPath, string fragmentPath)
+        //{
+        //    ////
+        //    //string VertexShaderSource;
+
+        //    //using (StreamReader reader = new StreamReader(vertexPath, Encoding.UTF8))
+        //    //{
+        //    //    VertexShaderSource = reader.ReadToEnd();
+        //    //}
+
+        //    //string FragmentShaderSource;
+
+        //    //using (StreamReader reader = new StreamReader(fragmentPath, Encoding.UTF8))
+        //    //{
+        //    //    FragmentShaderSource = reader.ReadToEnd();
+        //    //}
+
+        //    ////
+        //    //_vertexShader = GL.CreateShader(ShaderType.VertexShader);
+        //    //GL.ShaderSource(_vertexShader, VertexShaderSource);
+
+        //    //_fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+        //    //GL.ShaderSource(_fragmentShader, FragmentShaderSource);
+
+        //    ////
+        //    //GL.CompileShader(_vertexShader);
+
+        //    //string infoLogVert = GL.GetShaderInfoLog(_vertexShader);
+        //    //if (infoLogVert != System.String.Empty)
+        //    //    System.Console.WriteLine(infoLogVert);
+
+        //    //GL.CompileShader(_fragmentShader);
+
+        //    //string infoLogFrag = GL.GetShaderInfoLog(_fragmentShader);
+
+        //    //if (infoLogFrag != System.String.Empty)
+        //    //    System.Console.WriteLine(infoLogFrag);
+
+        //    ////
+        //    //Handle = GL.CreateProgram();
+
+        //    //GL.AttachShader(Handle, _vertexShader);
+        //    //GL.AttachShader(Handle, _fragmentShader);
+
+        //    //GL.LinkProgram(Handle);
+
+        //    ////
+        //    //GL.DetachShader(Handle, _vertexShader);
+        //    //GL.DetachShader(Handle, _fragmentShader);
+        //    //GL.DeleteShader(_fragmentShader);
+        //    //GL.DeleteShader(_vertexShader);
+        //}
+
+
+        public Shader(string vertexPath, string fragPath)
+        {
+            //Load vertex shader and compile
+            var shaderSource = LoadSource(vertexPath);
+
+            var vertexShader = GL.CreateShader(ShaderType.VertexShader);
+
+            GL.ShaderSource(vertexShader, shaderSource);
+
+            CompileShader(vertexShader);
+
+            //Load fragment shader and compile
+            shaderSource = LoadSource(fragPath);
+            var fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShader, shaderSource);
+            CompileShader(fragmentShader);
+
+            //
+            Handle = GL.CreateProgram();
+            GL.AttachShader(Handle, vertexShader);
+            GL.AttachShader(Handle, fragmentShader);
+
+            LinkProgram(Handle);
+
+            //
+            GL.DetachShader(Handle, vertexShader);
+            GL.DetachShader(Handle, fragmentShader);
+            GL.DeleteShader(fragmentShader);
+            GL.DeleteShader(vertexShader);
+
+            //
+            GL.GetProgram(Handle, GetProgramParameterName.ActiveUniforms, out var numberOfUniforms);
+
+            //
+            _uniformLocations = new Dictionary<string, int>();
+
+            // Loop over all the uniforms,
+            for (var i = 0; i < numberOfUniforms; i++)
+            {
+                // get the name of this uniform,
+                var key = GL.GetActiveUniform(Handle, i, out _, out _);
+
+                // get the location,
+                var location = GL.GetUniformLocation(Handle, key);
+
+                // and then add it to the dictionary.
+                _uniformLocations.Add(key, location);
+            }
+        }
+
+        private void LinkProgram(int program)
+        {
+            // We link the program
+            GL.LinkProgram(program);
+
+            // Check for linking errors
+            GL.GetProgram(program, GetProgramParameterName.LinkStatus, out var code);
+            if (code != (int)All.True)
+            {
+                // We can use `GL.GetProgramInfoLog(program)` to get information about the error.
+                throw new Exception($"Error occurred whilst linking Program({program})");
+            }
+        }
+
+        private void CompileShader(int Shader)
+        {
+            GL.CompileShader(Shader);
+
+            // Check for compilation errors
+            GL.GetShader(Shader, ShaderParameter.CompileStatus, out var code);
+            if (code != (int)All.True)
+            {
+                // We can use `GL.GetShaderInfoLog(shader)` to get information about the error.
+                throw new Exception($"Error occurred whilst compiling Shader({Shader})");
+            }
+        }
+
+        //
+        private string LoadSource(string vertexPath)
+        {
+            using (var sr = new StreamReader(vertexPath, Encoding.UTF8))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        //
+        public void Use()
+        {
+            GL.UseProgram(Handle);
+        }
+
+        //
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                GL.DeleteProgram(Handle);
+
+                disposedValue = true;
+            }
+        }
+
+        //~Shader()
+        //{
+        //    GL.DeleteProgram(Handle);
+        //}
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
